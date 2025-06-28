0cc9ddbcf3eaf3c684366ecabca682365154fda1
Tomi <tomiaiyeniko@penzack.com>
2025-06-28 01:09:26 +0100

    solution for question 3

---
+++ question_3/src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h3> Header </h3>{" "}
+      </div>{" "}
+      <div className="hero">
+        <h3> Hero </h3>{" "}
+      </div>{" "}
+      <div className="main">
+        <h3> Main Content </h3>{" "}
+        <p> This is the main content, lorem ipsum lorem ipsum </p>{" "}
+      </div>{" "}
+      <div className="side">
+        <h3> Sidebar </h3>{" "}
+      </div>{" "}
+      <div className="extra">
+        <h3> Extra Content </h3>{" "}
+      </div>{" "}
+      <div className="imgs">
+        <h3> Related Images </h3>{" "}
+      </div>{" "}
+      <div className="posts">
+        <h3> Related Posts </h3>{" "}
+      </div>{" "}
+      <div className="footer">
+        <h3> Footer </h3>{" "}
+      </div>{" "}
+    </div>
+  );
+}
